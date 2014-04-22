40057eafee0eace07e713499ef11cc58cc012aab
Michel Hognerud <leyou.m@gmail.com>
2014-04-22 12:26:31 +0800

    fixes

--- Tests/BehaviorsTest.php
+++ Tests/BehaviorsTest.php
@@ -1,30 +1,36 @@
 <?php
-class BehaviorsTest extends PHPUnit_Framework_TestCase {
+namespace Asgard\Behaviors\Tests;
+
+class BehaviorsTest extends \PHPUnit_Framework_TestCase {
 	public static function setUpBeforeClass() {
 		if(!defined('_ENV_'))
 			define('_ENV_', 'test');
 		require_once(_CORE_DIR_.'core.php');
 
 		\Asgard\Core\App::instance(true)->config->set('bundles', array(
-			_ASGARD_DIR_.'core',
 			_ASGARD_DIR_.'behaviors',
 		));
 		\Asgard\Core\App::loadDefaultApp();
+
+		\Asgard\Core\App::get('hook')->hook('behaviors_pre_load', function($chain, $entityDefinition) {
+			if(!isset($entityDefinition->behaviors['Asgard\Behaviors\TimestampsBehavior']))
+				$entityDefinition->behaviors['Asgard\Behaviors\TimestampsBehavior'] = true;
+		});
 	}
 	
 	#page
 	public function test1() {
-		$this->assertTrue(Asgard\Behaviors\Tests\Entities\News::getDefinition()->behaviors['Asgard\Behaviors\MetasBehavior']);
+		$this->assertTrue(\Asgard\Behaviors\Tests\Entities\News::getDefinition()->behaviors['Asgard\Behaviors\MetasBehavior']);
 		// $this->assertTrue(Asgard\Behaviors\Tests\Entities\News::getDefinition()->behaviors['Asgard\Behaviors\SlugifyBehavior']);
 	}
 
 	#metas
 	public function test2() {
-		$this->assertTrue(Asgard\Behaviors\Tests\Entities\News::hasProperty('meta_title'));
-		$this->assertTrue(Asgard\Behaviors\Tests\Entities\News::hasProperty('meta_description'));
-		$this->assertTrue(Asgard\Behaviors\Tests\Entities\News::hasProperty('meta_keywords'));
+		$this->assertTrue(\Asgard\Behaviors\Tests\Entities\News::hasProperty('meta_title'));
+		$this->assertTrue(\Asgard\Behaviors\Tests\Entities\News::hasProperty('meta_description'));
+		$this->assertTrue(\Asgard\Behaviors\Tests\Entities\News::hasProperty('meta_keywords'));
 
-		$news = new Asgard\Behaviors\Tests\Entities\News(array(
+		$news = new \Asgard\Behaviors\Tests\Entities\News(array(
 			'title' => 'Test',
 			'content' => 'Test',
 			'meta_title' => 'Test Meta Title',
@@ -41,8 +47,8 @@ class BehaviorsTest extends PHPUnit_Framework_TestCase {
 
 	#slugify
 	public function test3() {
-		$this->assertTrue(Asgard\Behaviors\Tests\Entities\News::hasProperty('slug'));
-		$news = new Asgard\Behaviors\Tests\Entities\News(array(
+		$this->assertTrue(\Asgard\Behaviors\Tests\Entities\News::hasProperty('slug'));
+		$news = new \Asgard\Behaviors\Tests\Entities\News(array(
 			'title' => 'Test Title',
 			'content' => 'Test Content',
 		));
@@ -53,17 +59,17 @@ class BehaviorsTest extends PHPUnit_Framework_TestCase {
 
 	#timestamps
 	public function test4() {
-		$this->assertTrue(Asgard\Behaviors\Tests\Entities\News::hasProperty('created_at'));
-		$this->assertTrue(Asgard\Behaviors\Tests\Entities\News::hasProperty('updated_at'));
+		$this->assertTrue(\Asgard\Behaviors\Tests\Entities\News::hasProperty('created_at'));
+		$this->assertTrue(\Asgard\Behaviors\Tests\Entities\News::hasProperty('updated_at'));
 
-		$news = new Asgard\Behaviors\Tests\Entities\News(array(
+		$news = new \Asgard\Behaviors\Tests\Entities\News(array(
 			'title' => 'Test Title',
 			'content' => 'Test Content',
 		));
 		$this->assertEquals(date('d/m/Y H:i:s'), $news->created_at->format('d/m/Y H:i:s'));
 		$this->assertEquals(date('d/m/Y H:i:s'), $news->updated_at->format('d/m/Y H:i:s'));
 
-		$news = new Asgard\Behaviors\Tests\Entities\News(array(
+		$news = new \Asgard\Behaviors\Tests\Entities\News(array(
 			'title' => 'Test Title',
 			'content' => 'Test Content',
 			'created_at' => '2009-09-09 09:09:09',
